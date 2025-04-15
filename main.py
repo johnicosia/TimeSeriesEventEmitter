48cbe0ecc5f29b616f16d72484c8bf45401061fc
johnicosia <john.c.nicosia@gmail.com>
2025-04-15 12:07:30 -0700

    add history (in memory) endpoint
    add support for history endpoint using in memory storage. The endpoint supports getting the last N events. The query parameter last has a default value of 10.

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import uvicorn
 
 app = FastAPI()
 
+events = []
+
 async def generate_tempature_events(interval):
     temperature = 70
     while True:
@@ -20,6 +22,7 @@ async def generate_tempature_events(interval):
         tempature_difference = random.random() if tempature_goes_up else (random.random() * -1)
         temperature = round(temperature + tempature_difference, 2)
         data["value"] = temperature
+        events.append(data)
         yield {"data": json.dumps(data)}
         # wait an interval before repeating the loop
         await asyncio.sleep(interval)
@@ -27,6 +30,10 @@ async def generate_tempature_events(interval):
 @app.get("/stream")
 async def get_timeseries_data(interval: int = 1):
     return EventSourceResponse(generate_tempature_events(interval))
+
+@app.get("/history")
+async def get_history(last: int = 10):
+    return events[-last:][::-1]
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000, reload=False)
